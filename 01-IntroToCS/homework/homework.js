02c53c5d38883fce166cdc3663eb5c7d8bbe3bd3
Marcia Hernández <ht.marcia@gmail.com>
2023-02-27 17:35:52 -0600

    1st homework solved

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,48 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var array = String(num).split("").map((num)=>{
+      return Number(num)
+    })
 
-function DecimalABinario(num) {}
+   var counter = 0;
+   var newArray = [];
+   
+   for (let i = array.length - 1; i > -1; i--) {
+      const element = array[i];
+      if (element === 1) {
+         var pow = Math.pow(2, counter);
+         newArray.push(pow);
+      }
+      counter += 1;
+   }
+
+   var decimal = newArray.reduce((a, b) => a + b, 0);
+   return decimal;
+}
+
+function DecimalABinario(num) {
+   
+   var workingNumber = num;
+   var array = []; 
+
+   while (workingNumber >= 1) {
+      if (workingNumber % 2 === 0) {
+         array.push(0);
+      }else{
+         array.push(1);
+      }
+      workingNumber = Math.floor(workingNumber/2);
+      console.log(workingNumber);
+   }
+   array.reverse();
+
+   return array.join('');
+}
+
 
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
 };
+
