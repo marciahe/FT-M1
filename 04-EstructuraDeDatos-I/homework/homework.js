58693c102c5e2d9912c11c671128c5ca9d333239
Marcia Hernández <ht.marcia@gmail.com>
2023-03-02 16:53:24 -0600

    Homework 3 ready

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,9 +14,22 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n < 2) {return 1}
+  // console.log(n);
+  return n * nFactorial(n-1);
+}
+// nFactorial(4);
 
-function nFibonacci(n) {}
+function nFibonacci(n) {
+  if (n === 0){return 0}
+  if (n === 1 || n === 2){return 1}
+  console.log(n);
+  return nFibonacci(n-1) + nFibonacci(n-2);
+   
+}
+nFibonacci(3);
+
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +40,26 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.arr = []
+}
+Queue.prototype.enqueue = function(value){
+  return this.arr.push(value);
+}
+Queue.prototype.dequeue = function(){
+  return this.arr.shift();
+}
+Queue.prototype.size = function(){
+  return this.arr.length;
+}
+
+var queue = new Queue();
+
+queue.enqueue('first');
+queue.enqueue('second');
+queue.enqueue('third');
+queue.dequeue();
+
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
